641523b04291530f9ff0f57ffe11d99948457d6e
Sandra <sun.is.invisible@gmail.com>
2014-01-20 18:44:51 +0100

    Zmena group(1) za konkretny nazov grupy, tj Mnozstvo

--- reg.py
+++ reg.py
@@ -4,9 +4,9 @@ import sys
 
 def kurz(mena):
     html_content = urllib2.urlopen('http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.jsp').read()
-    text = re.search(r'(\d+)</td><td>' + mena + r'</td><td align="right">(\d+,\d+)</td>', html_content)
-    mnozstvo = float(text.group(1))
-    kurz = float(text.group(2).replace(",", "."))
+    text = re.search(r'(?P<Mnozstvo>\d+)</td><td>' + mena + r'</td><td align="right">(?P<Kurz>\d+,\d+)</td>', html_content)
+    mnozstvo = float(text.group('Mnozstvo'))
+    kurz = float(text.group('Kurz').replace(",", "."))
     return kurz/mnozstvo
 
 print kurz(sys.argv[1])
