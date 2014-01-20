6241526681d1754a2eda12d686f4195f12ebd2ea
Sandra <sun.is.invisible@gmail.com>
2014-01-20 18:40:07 +0100

    Vyhladame si mnozstvo u meny aby sme ziskali kurz na jednu korunu

--- reg.py
+++ reg.py
@@ -1,15 +1,13 @@
 import re
 import urllib2
+import sys
 
 def kurz(mena):
     html_content = urllib2.urlopen('http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.jsp').read()
+    text = re.search(r'(\d+)</td><td>' + mena + r'</td><td align="right">(\d+,\d+)</td>', html_content)
+    mnozstvo = float(text.group(1))
+    kurz = float(text.group(2).replace(",", "."))
+    return kurz/mnozstvo
 
-    #print html_content
-    
-    return re.search('<td>' + mena + r'</td><td align="right">(\d+,\d+)</td>', html_content).group(1)
-
-
-print kurz('USD')
-
-
+print kurz(sys.argv[1])
 
