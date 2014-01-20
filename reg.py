58ee61cd68f8c05b3f29f1dabe4b2d62d3c1ce8a
Sandra <sun.is.invisible@gmail.com>
2014-01-20 19:53:01 +0100

    Pridanie krajiny a názvu meny, oprava kódovania v prvom riadku

--- reg.py
+++ reg.py
@@ -1,3 +1,5 @@
+# Encoding: UTF-8
+
 import re
 import urllib2
 import sys
@@ -10,13 +12,15 @@ else:
     def kurz(mena):
         html_content = urllib2.urlopen('http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.jsp').read()
         # re.escape vsetky specialne znaky opatri lomitkom, aby sa nebrali ako regularne vyrazy
-        text = re.search(r'(?P<Mnozstvo>\d+)</td><td>' + re.escape(mena) + r'</td><td align="right">(?P<Kurz>\d+,\d+)</td>', html_content)
+        text = re.search(r'<td>(?P<Stat>[^<]+)</td><td>(?P<Nazov_meny>[^<]+)</td><td align="right">(?P<Mnozstvo>\d+)</td><td>' + re.escape(mena) + r'</td><td align="right">(?P<Kurz>\d+,\d+)</td>', html_content)
         if text == None:
             print "Mena " + sys.argv[1] + " nie je v zozname!"
         else:
+            stat = text.group('Stat')
+            nazov_meny = text.group('Nazov_meny')
             mnozstvo = float(text.group('Mnozstvo'))
             kurz = float(text.group('Kurz').replace(",", "."))
-            return kurz/mnozstvo
+            return '{stat}: {mnozstvo} {mena}({nazov_meny})={kurz} Kč'.format(stat=stat, mnozstvo=mnozstvo, mena=sys.argv[1].upper(), nazov_meny=nazov_meny, kurz=kurz)
 
-    print kurz(sys.argv[1])
+    print kurz(sys.argv[1].upper())
 
